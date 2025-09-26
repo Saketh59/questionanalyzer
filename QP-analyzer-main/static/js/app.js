00be21862ad766c5faecfbef838e25f6a43fef1e
sai saketh <saisaketh.tallam@gmail.com>
2025-09-26 22:20:27 +0530

    Initial commit

--- QP-analyzer-main/static/js/app.js
+++
@@ -1,9 +0,0 @@
-// Simple navigation from landing to dashboard (Flask route)
-(function () {
-  const startBtn = document.getElementById('startBtn');
-  if (startBtn) {
-    startBtn.addEventListener('click', () => {
-      window.location.href = '/dashboard';
-    });
-  }
-})();
